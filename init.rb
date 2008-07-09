0fdbe54a4d99a41287bfcb360d8287eaa978900f
tiendung <tiendung@798b37cb-fcb3-7149-94e7-c2420af4a3b0>
2008-07-09 08:03:18 +0000

    add hunspell gem to acts_as_dictionary
    git-svn-id: svn://203.125.43.235/blossoms/vendor/plugins/acts_as_dictionary@40764 798b37cb-fcb3-7149-94e7-c2420af4a3b0

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
+require File.join(File.dirname(__FILE__),  "hunspell/Hunspell.so")
+
 ActiveRecord::Base.send(:include, ActsAsDictionary) 
--- lib/acts_as_dictionary.rb
+++ lib/acts_as_dictionary.rb
@@ -1,5 +1,3 @@
-require 'Hunspell'
-
 module ActsAsDictionary
   DICT_ROOT = File.join(RAILS_ROOT, "dict")
   
