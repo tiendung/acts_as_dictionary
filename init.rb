951cbbaa527039c4e984e36a6da45715de3ff582
tiendung <tiendung@798b37cb-fcb3-7149-94e7-c2420af4a3b0>
2008-07-09 09:13:09 +0000

    use hunspell gem as default
    git-svn-id: svn://203.125.43.235/blossoms/vendor/plugins/acts_as_dictionary@40771 798b37cb-fcb3-7149-94e7-c2420af4a3b0

--- init.rb
+++ init.rb
@@ -1,3 +1,11 @@
-require File.join(File.dirname(__FILE__),  "hunspell/Hunspell.so")
+begin
+  require "hunspell"
+rescue LoadError
+  $stderr.puts "Load build-in hunspell lib"
+  require File.join(File.dirname(__FILE__),  "hunspell/Hunspell.so")
+rescue LoadError
+  $stderr.puts "run rake dict:install to make hunspell lib"
+  exit 1
+end
 
 ActiveRecord::Base.send(:include, ActsAsDictionary) 
