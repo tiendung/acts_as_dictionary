6a1619d89b573e116bc76e1862708e9758da7061
tiendung <tiendung@798b37cb-fcb3-7149-94e7-c2420af4a3b0>
2008-07-11 17:14:08 +0000

    better notice
    git-svn-id: svn://203.125.43.235/blossoms/vendor/plugins/acts_as_dictionary@40840 798b37cb-fcb3-7149-94e7-c2420af4a3b0

--- init.rb
+++ init.rb
@@ -2,13 +2,11 @@ begin
  require "rubygems"
  require "hunspell"
 rescue LoadError
- $stderr.puts "Load build-in hunspell lib"
+ $stderr.puts "Load hunspell lib"
  require File.join(File.dirname(__FILE__),  "hunspell/Hunspell.so")
 rescue LoadError
  $stderr.puts "run rake dict:install to make hunspell lib"
  exit 1
 end
-
-# require File.join(File.dirname(__FILE__),  "hunspell/Hunspell.so")
 
 ActiveRecord::Base.send(:include, ActsAsDictionary) 
