9c273c902f8e70a6e12b49884dd17857ff9bb36c
tiendung <tiendung@798b37cb-fcb3-7149-94e7-c2420af4a3b0>
2008-08-04 04:33:41 +0000

    remove hunspell gem
    git-svn-id: svn://203.125.43.235/blossoms/vendor/plugins/acts_as_dictionary@42244 798b37cb-fcb3-7149-94e7-c2420af4a3b0

--- init.rb
+++ init.rb
@@ -1,12 +1,2 @@
-begin
- require "rubygems"
- require "hunspell"
-rescue LoadError
- $stderr.puts "Load hunspell lib"
- require File.join(File.dirname(__FILE__),  "hunspell/Hunspell.so")
-rescue LoadError
- $stderr.puts "run rake dict:install to make hunspell lib"
- exit 1
-end
-
+require "hunspell"
 ActiveRecord::Base.send(:include, ActsAsDictionary) 
