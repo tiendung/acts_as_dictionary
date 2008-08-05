0e9731d4fc71c28e39c290ecd45f3b81d3eced9c
tiendung <tiendung@798b37cb-fcb3-7149-94e7-c2420af4a3b0>
2008-08-05 04:32:55 +0000

    more robust hunspell binding
    git-svn-id: svn://203.125.43.235/blossoms/vendor/plugins/acts_as_dictionary@42262 798b37cb-fcb3-7149-94e7-c2420af4a3b0

--- lib/hunspell.rb
+++ lib/hunspell.rb
@@ -1,9 +1,3 @@
-##
-# KISS + Hunspell (Keep It Simple but no Simpler) 
-# => Hunspell binding within 60 lines (thanks to RubyInline :)
-#
-# Nguyen Tien Dung (dungtn@gmail.com)
-
 require 'rubygems'
 require 'inline'
 
@@ -11,61 +5,62 @@ class Hunspell
   inline do |builder|
     builder.add_link_flags("-lhunspell-1.2")
     
-    builder.prefix <<-EOC
+    builder.prefix <<-HUNSPELL_H
       typedef struct Hunhandle Hunhandle;
-      Hunhandle *Hunspell_create(const char * affpath, const char * dpath);
-      void Hunspell_destroy(Hunhandle *pHunspell);
-
-      int Hunspell_spell(Hunhandle *pHunspell, const char *);
-      int Hunspell_suggest(Hunhandle *pHunspell, char*** slst, const char * word);
-    EOC
+      Hunhandle *Hunspell_create  (const char *affpath, const char *dicpath);
+            void Hunspell_destroy (Hunhandle *pHunspell);
+             int Hunspell_spell   (Hunhandle *pHunspell, const char *word);
+             int Hunspell_suggest (Hunhandle *pHunspell, char ***slst, const char *word);
+    HUNSPELL_H
     
-    builder.c_singleton <<-EOC
-      VALUE new(const char* affpath, const char* dpath) {
+    builder.prefix <<-GET_HUNHANDLE
+      static Hunhandle *get_hunhandle(VALUE klass) {
         Hunhandle *pHunspell;
-        pHunspell = Hunspell_create(affpath, dpath);
-
-        if (!pHunspell) {
+        Data_Get_Struct(klass, Hunhandle, pHunspell);
+        if (pHunspell == NULL) {
+          rb_raise(rb_eRuntimeError, "Something wrong with wrapped Hunspell handle.");
+        }
+        return pHunspell;
+      }
+    GET_HUNHANDLE
+    
+    builder.c_singleton <<-HUNSPELL_NEW
+      VALUE new(const char* affpath, const char* dicpath) {
+        Hunhandle *pHunspell = Hunspell_create(affpath, dicpath);
+        if (pHunspell == NULL) {
           rb_raise(rb_eRuntimeError, "Failed to initialize Hunspell.");
-        }    
-
+        }
         return Data_Wrap_Struct(self, 0, Hunspell_destroy, pHunspell);
       }
-    EOC
+    HUNSPELL_NEW
 
-    builder.c <<-EOC
+    builder.c <<-HUNSPELL_CHECK
       VALUE check(const char* str) {
-        Hunhandle *pHunspell;
-        Data_Get_Struct(self, Hunhandle, pHunspell);
-
-        return (Hunspell_spell(pHunspell, str) == 1 ? Qtrue : Qfalse);
+        return (Hunspell_spell(get_hunhandle(self), str) == 1 ? Qtrue : Qfalse);
       }
-    EOC
+    HUNSPELL_CHECK
 
-    builder.c <<-EOC
-      VALUE suggest(const char* str) {
+    builder.c <<-HUNSPELL_SUGGEST
+      VALUE suggest(const char* word) {
         int i, n;
         char **list, *item;
         VALUE suggestions;
 
-        Hunhandle *pHunspell;
-        Data_Get_Struct(self, Hunhandle, pHunspell);
-        
-        n = Hunspell_suggest(pHunspell, &list, str);
+        n = Hunspell_suggest(get_hunhandle(self), &list, word);
         suggestions = rb_ary_new2(n);
         
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < n; ++i) {
           item = list[i];
           rb_ary_push(suggestions, rb_str_new2(item));
           free(item);
         }
 
-        if (n > 0) { 
+        if (n > 0) {
           free(list); 
         }
-
+        
         return suggestions;
       }
-    EOC
+    HUNSPELL_SUGGEST
   end
 end
